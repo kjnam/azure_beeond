11a857d461e8cd56378e786ebd8d4381082a5de9
Kijin Nam <knam@water.ca.gov>
2023-05-25 22:43:00 +0000

    Add some script to use BeeOND on Azure

---
+++ configure_local_nvme_disks.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -xe
+
+if [ ! -d /mnt/resource_nvme ]; then
+    mkdir /mnt/resource_nvme
+fi
+# mdadm --create /dev/md10 --level 0 --raid-devices 2 /dev/nvme0n1 /dev/nvme1n1
+parted /dev/nvme0n1 --script mklabel gpt mkpart xfspart xfs 0% 100%
+partprobe /dev/nvme0n1p1
+mkfs.xfs /dev/nvme0n1p1
+# mount /dev/md10 /mnt/resource_nvme
+mount /dev/nvme0n1p1 /mnt/resource_nvme
+chmod 1777 /mnt/resource_nvme
---
+++ setupbeeond.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -xe
+MOUNT_ROOT=${1:-/mnt/resource_nvme}
+# this script needs to run without sudo
+#  - the keys from the user are used for the root user
+
+sudo wget -O /etc/yum.repos.d/beegfs-rhel8.repo https://www.beegfs.io/release/beegfs_7.3.3/dists/beegfs-rhel8.repo
+sudo rpm --import https://www.beegfs.io/release/beegfs_7.3.3/gpg/GPG-KEY-beegfs
+
+sudo yum install -y epel-release
+sudo yum install -y psmisc libbeegfs-ib beeond pdsh
+
+sudo sed -i 's/^buildArgs=-j8/buildArgs=-j8 BEEGFS_OPENTK_IBVERBS=1 OFED_INCLUDE_PATH=\/usr\/src\/ofa_kernel\/default\/include/g' /etc/beegfs/beegfs-client-autobuild.conf
+
+sudo /etc/init.d/beegfs-client rebuild || exit 1
+
+sudo cp -r $HOME/.ssh /root/.
+
+sudo mkdir $MOUNT_ROOT/beeond
+sudo chmod 777 $MOUNT_ROOT/beeond
+sudo mkdir /beeond
+sudo chmod 777 /beeond
