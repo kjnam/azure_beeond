ed614ec803841aa86a4b0a0be4c43950307e6f95
Kijin Nam <knam@water.ca.gov>
2023-05-26 22:11:14 +0000

    Add rcmd

--- setupbeeond.sh
+++ setupbeeond.sh
@@ -8,7 +8,7 @@ sudo wget -O /etc/yum.repos.d/beegfs-rhel8.repo https://www.beegfs.io/release/be
 sudo rpm --import https://www.beegfs.io/release/beegfs_7.3.3/gpg/GPG-KEY-beegfs
 
 sudo yum install -y epel-release
-sudo yum install -y psmisc libbeegfs-ib beeond pdsh
+sudo yum install -y psmisc libbeegfs-ib beeond pdsh pdsh-rcmd-ssh
 
 sudo sed -i 's/^buildArgs=-j8/buildArgs=-j8 BEEGFS_OPENTK_IBVERBS=1 OFED_INCLUDE_PATH=\/usr\/src\/ofa_kernel\/default\/include/g' /etc/beegfs/beegfs-client-autobuild.conf
 
